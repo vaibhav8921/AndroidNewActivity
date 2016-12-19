85f70d5d9d50362d738ce780bbecec88064c4098
Vaibhav <vaibhav8921@gmail.com>
2016-12-18 20:31:38 -0800

    Ashwani will solve the problem

--- app/src/main/java/androidsample/mycompany/com/androidnewactivity/Display.java
+++ app/src/main/java/androidsample/mycompany/com/androidnewactivity/Display.java
@@ -2,7 +2,6 @@ package androidsample.mycompany.com.androidnewactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 /**
  * Created by Vaibhav on 11/27/2016.
@@ -14,8 +13,7 @@ public class Display extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
-        String userName= getIntent().getStringExtra("UserName");
-        TextView tv = (TextView)findViewById(R.id.TVUserName);
-        tv.setText(userName);
+   /*     String userName= getIntent().getStringExtra("UserName");
+        TextView tv = (TextView)findViewById(R.id.TVUserName);*/
     }
 }
